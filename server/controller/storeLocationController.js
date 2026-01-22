75e17da3e7224962de1fa72815376c20fa2d4d85
“Victor <abimbolavictor3@gmail.com>
2026-01-22 19:18:09 +0100

    fixs

--- server/controller/storeLocationController.js
+++ server/controller/storeLocationController.js
@@ -38,33 +38,20 @@ export const getStoresClient = async (req,res) => {
     }
 }
 
-export const getStores = async (req, res) => {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-
+export const getStores = async (req,res) => {
     try {
-        // Filter for only ACTIVE products
-
-        const store = await Store.find({})
-            .select('name address postalcode country state status') // Limit fields
-            .populate('address', 'name') // 👈 populate only _id and name of category
-            .skip(skip)
-            .limit(limit)
-            .sort({ createdAt: -1 });
-
-        const count = await Store.countDocuments(filter);
-
+        const filter = {};
+        const store = await Store.find(filter)
         res.status(200).json({
-            total: count,
-            page,
-            pages: Math.ceil(count / limit),
-            store, // 👈 category will be like: { _id, name }
+
+            store,
         });
     } catch (err) {
         res.status(500).json({ message: 'Server Error', error: err.message });
     }
-};
+}
+
+
 
 
 
